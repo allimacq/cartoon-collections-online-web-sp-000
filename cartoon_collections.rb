44917aae85e80d3c2751617f49a7e601657576cb
Allison MacQueen <allimacq@gmail.com>
2020-08-31 00:41:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each.with_index(1) { |n, index| puts "#{index}. #{n}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  final = [ ]
+  planeteer_calls.collect {|call| final << "#{call.capitalize}!" }
+  final
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(five)
+   five.each do |x|
+    if x.match(/\w{5,}/)
+      return false
+    elsif x.match(/\w{0,4}/)
+      return true
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese#()
+
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
