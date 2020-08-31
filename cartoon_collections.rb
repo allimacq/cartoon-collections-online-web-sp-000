b992f1743b550b554b22ed966374782f64a0b0b5
Allison MacQueen <allimacq@gmail.com>
2020-08-31 00:55:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,12 +9,10 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(five)
-   five.each do |x|
-    if x.any?(/\w{5,}/)
-      return true
-    elsif x.all?(/\b\w{1,4}\b/)
-      return false
-    end
+  if five.any?(/\w{5,}/)
+    return true
+  elsif x.all?(/\b\w{1,4}\b/)
+    return false
   end
 end
 
